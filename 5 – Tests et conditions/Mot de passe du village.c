39ba8716abf0e24d7bb31fbf1f03bba94488ec5e
Juan Valero <159065266+juanvlro@users.noreply.github.com>
2024-10-31 22:20:26 +0100

    Resolve Mot de passe du village

---
+++ 5 – Tests et conditions/Mot de passe du village.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int code;
+   scanf("%d", &code);
+   
+   if (code == 64741)
+   {
+      printf("Bon festin !\n");
+   }
+   else
+   {
+      printf("Allez-vous en !\n");
+   }
+}
