97a9bd11b105e4b99e5cbb7f3ee2ef048f5284cb
Juan Valero <159065266+juanvlro@users.noreply.github.com>
2024-10-31 21:33:44 +0100

    Resolve Âge des petits-enfants

---
+++ 4 - Lecture de l'entrée/Âge des petits-enfants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   int ageCadet;
+   int ageAine;
+   scanf("%d", &ageCadet);
+   scanf("%d", &ageAine);  
+   int difference = ageAine - ageCadet;
+   printf("%d\n", difference);
+}
