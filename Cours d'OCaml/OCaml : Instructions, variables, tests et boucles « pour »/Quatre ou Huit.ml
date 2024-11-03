fa432024919ed769362bfd17bf3a7dc57a9f7d1f
Juan Valero <159065266+juanvlro@users.noreply.github.com>
2024-11-03 16:21:31 +0100

    Resolve Quatre ou Huit

---
+++ Cours d'OCaml/OCaml : Instructions, variables, tests et boucles « pour »/Quatre ou Huit.ml
@@ -0,0 +1,12 @@
+let n = read_int() in
+
+if n = 4
+   then print_string "c'est 4"
+   else
+      begin
+      if n = 8
+         then print_string "c'est 8"
+         else print_string "autre valeur"
+      ;
+      end
+   ;
